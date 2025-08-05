77552656e92f4ffaf1bcf43f805388f982029e8f
grouvie <20428462+grouvie@users.noreply.github.com>
2025-08-06 00:20:09 +0200

    Add PREMIUM_USERS

--- src/Messages/TarotMessage.ts
+++ src/Messages/TarotMessage.ts
@@ -1,20 +1,50 @@
-import {PrivateMessage} from "./PrivateMessage";
-import {RawMessage} from "./RawMessage";
+import { PrivateMessage } from "./PrivateMessage";
+import { RawMessage } from "./RawMessage";
+import { TarotService } from "../Tarot/TarotService";
+import axios from "axios";
 
 export const tarotCommandIdentifier: string[] = ["!tech-tarot", "!tt"];
 
+const PREMIUM_USERS: string[] = ["grouvie", "anomalienchen"];
+
 export class TarotMessage implements Message {
-    username: string;
-    command: string;
+  username: string;
+  command: string;
 
-    constructor(message: RawMessage) {
-        let parts: string[] = message.content.message.split(' ');
-        this.username = message.content.prefix.nickname
-        this.command = parts[0];
-    }
+  constructor(message: RawMessage) {
+    let parts: string[] = message.content.message.split(" ");
+    this.username = message.content.prefix.nickname;
+    this.command = parts[0];
+  }
 
-    async answer(): Promise<string> {
-        let answer = 'Nutze deine Kartoffelherzen um deine Tech-Zukunft zu erfahren!';
-        return new PrivateMessage(answer).content;
+  async answer(): Promise<string> {
+    if (PREMIUM_USERS.includes(this.username.toLowerCase())) {
+      return await this.initializeTechTarotSession();
+    } else {
+      let answer =
+        "Nutze deine Kartoffelherzen um deine Tech-Zukunft zu erfahren!";
+      return new PrivateMessage(answer).content;
     }
+  }
+  private async startTarot(): Promise<boolean> {
+    TarotService.startTimer();
+    try {
+      const response = await axios.post("http://localhost:8080/start", {
+        user: this.username,
+      });
+      return response.status === 200;
+    } catch (err) {
+      return false;
+    }
+  }
+  private async initializeTechTarotSession(): Promise<string> {
+    let answer = "Die Zukunft kann gerade nicht";
+    if (!TarotService.isSessionActive()) {
+      const successful = await this.startTarot();
+      answer = successful
+        ? `Deine Tech-Zukunft erfährst du jetzt ${this.username}!`
+        : "Die Zukunft hat gerade geschlossen!";
+    }
+    return new PrivateMessage(answer).content;
+  }
 }
